74018b4503372d02cb732d23278ee2793038bbb6
Michael Vincent Rayo <28614088+mchlvncntry@users.noreply.github.com>
2019-02-05 03:10:30 -0800

    Update HeadTailListInterface.java

--- HeadTailListInterface.java
+++ HeadTailListInterface.java
@@ -9,8 +9,8 @@ public interface HeadTailListInterface<T>
 {
     /**
      * Adds a new entry to the beginning of this list.
-     * Entries currently in the list shall shift up to the next higher
-     * index. The list's size is increased by 1.
+     * Entries currently in the list shall shift up to the next higher index.
+     * The size of this list is increased by 1.
      * @param newEntry The object to be added as a new entry.
      * @throws ClassCastException if the class of the new entry
      * prevents it from being added to this list.
@@ -22,7 +22,7 @@ public interface HeadTailListInterface<T>
     /**
      * Adds a new entry to the end of this list.
      * Entries currently in this list are unaffected.
-     * The list's size is increased by 1.
+     * The size of this list is increased by 1.
      * @param newEntry The object to be added as a new entry.
      * @throws ClassCastException if the class of the new entry prevents it
      * from being added to this list.
@@ -34,7 +34,7 @@ public interface HeadTailListInterface<T>
     /**
      * Deletes and returns the entry from the beginning of this list.
      * Other entries' indexes are shifted up by 1.
-     * The list's size is decreased by 1.
+     * The size of this list is decreased by 1.
      * @return Either the object that has been deleted or null, if this list
      * is empty.
      */
@@ -43,7 +43,7 @@ public interface HeadTailListInterface<T>
     /**
      * Deletes and returns the entry from the end of this list.
      * Other entries currently in this list are unaffected.
-     * The list's size is decreased by 1.
+     * The size of this list is decreased by 1.
      * @return Either the object that has been deleted or null, if this list
      * is empty.
      */
@@ -67,12 +67,12 @@ public interface HeadTailListInterface<T>
     public int contains(T entry);
 
     /** Detects whether this list is empty.
-     * @return  True if this list contains no elements, false otherwise.
+     * @return True if this list contains no elements, false otherwise.
      */
     public boolean isEmpty();
 
     /**
-     * Returns the number of elements in the list.
+     * Detects the number of elements in this list.
      * @return The integer number of entries currently in this list.
      */
     public int size();
