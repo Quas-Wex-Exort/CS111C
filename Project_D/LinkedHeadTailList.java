0c92f49faf2a44146738724a27041d476a7dc804
Michael Vincent Rayo <28614088+mchlvncntry@users.noreply.github.com>
2019-03-31 16:41:47 -0700

    Update LinkedHeadTailList.java

--- Project_D/LinkedHeadTailList.java
+++ Project_D/LinkedHeadTailList.java
@@ -77,7 +77,6 @@ public class LinkedHeadTailList<T> implements HeadTailListInterface<T> {
         while (currentNode != null) {
             count++;
             currentNode = currentNode.next;
-
         }
         return count;
     }
@@ -89,28 +88,28 @@ public class LinkedHeadTailList<T> implements HeadTailListInterface<T> {
         private Node(T dataPortion) {
             data = dataPortion;
             next = null;
-        } // end constructor
+        }
 
         private Node(T dataPortion, Node nextNode) {
             data = dataPortion;
             next = nextNode;
-        } // end constructor
+        }
 
         private T getData() {
             return data;
-        } // end getData
+        }
 
         private void setData(T newData) {
             data = newData;
-        } // end setData
+        }
 
         private Node getNextNode() {
             return next;
-        } // end getNextNode
+        }
 
         private void setNextNode(Node nextNode) {
             next = nextNode;
-        } // end setNextNode
+        }
     } // end Node
 } 
 
