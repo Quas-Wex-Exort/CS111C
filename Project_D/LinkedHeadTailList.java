627e46b6407e0dff29f71b8423761a26749bd12c
Michael Vincent Rayo <28614088+mchlvncntry@users.noreply.github.com>
2019-04-04 11:06:03 -0700

    Update LinkedHeadTailList.java

--- Project_D/LinkedHeadTailList.java
+++ Project_D/LinkedHeadTailList.java
@@ -236,37 +236,34 @@ public class LinkedHeadTailList<T extends Comparable<? super T>> implements Head
         return Objects.hash(head, tail);
     }
     
-                
-    
     private class Node {
-        
-        private T data;
-        private Node next;
+		private T data; // Entry in list
+		private Node next; // Link to next node
 
-        private Node(T dataPortion) {
-            data = dataPortion;
-            next = null;
-        }
+		private Node(T dataPortion) {
+			data = dataPortion;
+			next = null;
+		} // end constructor
 
-        private Node(T dataPortion, Node nextNode) {
-            data = dataPortion;
-            next = nextNode;
-        }
+		private Node(T dataPortion, Node nextNode) {
+			data = dataPortion;
+			next = nextNode;
+		} // end constructor
 
-        private T getData() {
-            return data;
-        }
+		private T getData() {
+			return data;
+		} // end getData
 
-        private void setData(T newData) {
-            data = newData;
-        }
+		private void setData(T newData) {
+			data = newData;
+		} // end setData
 
-        private Node getNextNode() {
-            return next;
-        }
+		private Node getNextNode() {
+			return next;
+		} // end getNextNode
 
-        private void setNextNode(Node nextNode) {
-            next = nextNode;
-        }
-    } // end Node
+		private void setNextNode(Node nextNode) {
+			next = nextNode;
+		} // end setNextNode
+	} // end Node
 }
