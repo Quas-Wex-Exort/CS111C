7f1ae254e1962112c74cc1771a0f362308d034e7
MVR <28614088+mchlvncntry@users.noreply.github.com>
2019-05-07 02:53:06 -0700

    Update BinarySearchTreeWithDups.java

--- Project_E/BinarySearchTreeWithDups.java
+++ Project_E/BinarySearchTreeWithDups.java
@@ -3,19 +3,19 @@ import java.util.*;
 public class BinarySearchTreeWithDups<T extends Comparable<? super T>> extends BinarySearchTree<T> implements SearchTreeInterface<T>, java.io.Serializable {
 
 	public BinarySearchTreeWithDups() {
-		super();
+        super();
 	}
 
 	public BinarySearchTreeWithDups(T rootEntry) {
-		super(rootEntry);
-		setRootNode(new BinaryNode <T>(rootEntry));
+        super(rootEntry);
+        setRootNode(new BinaryNode <T>(rootEntry));
 	}
 
 	@Override
 	public T add(T newEntry) {
-		T result = newEntry;
-		if (isEmpty()) {
-			setRootNode(new BinaryNode <T>(newEntry));
+        T result = newEntry;
+        if (isEmpty()) {
+            setRootNode(new BinaryNode <T>(newEntry));
 		} else {
 			addEntryHelperNonRecursive(newEntry);
 		}
